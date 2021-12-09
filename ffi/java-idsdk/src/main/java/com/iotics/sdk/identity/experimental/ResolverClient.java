80e1b4bb2c073dc1ba2e4bb8224107770f8d90e3
smartrics <fabrizio.cannizzo@iotic-labs.com>
2021-12-09 18:54:46 +0000

    added method to java

--- ffi/java-idsdk/src/main/java/com/iotics/sdk/identity/experimental/ResolverClient.java
+++ ffi/java-idsdk/src/main/java/com/iotics/sdk/identity/experimental/ResolverClient.java
@@ -6,8 +6,11 @@ import okhttp3.Response;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.net.*;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
 import java.util.Base64;
+import java.util.Objects;
 
 public class ResolverClient {
     private final URL base;
@@ -19,9 +22,53 @@ public class ResolverClient {
         this.client = new OkHttpClient();
     }
 
-    public record Result(String content, String contentType, boolean isErr){};
+    public final class Result {
+        private final String content;
+        private final String contentType;
+        private final boolean isErr;
 
-    public Result discover(String did) throws IOException{
+        public Result(String content, String contentType, boolean isErr) {
+            this.content = content;
+            this.contentType = contentType;
+            this.isErr = isErr;
+        }
+
+        @Override
+        public String toString() {
+            return "Result{" +
+                    "content='" + content + '\'' +
+                    ", contentType='" + contentType + '\'' +
+                    ", isErr=" + isErr +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Result result = (Result) o;
+            return isErr == result.isErr && Objects.equals(content, result.content) && Objects.equals(contentType, result.contentType);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(content, contentType, isErr);
+        }
+
+        public String content() {
+            return content;
+        }
+
+        public String contentType() {
+            return contentType;
+        }
+
+        public boolean isErr() {
+            return isErr;
+        }
+    }
+
+    public Result discover(String did) throws IOException {
         URL url = null;
         try {
             url = new URL(base, "/1.0/discover/" + did);
