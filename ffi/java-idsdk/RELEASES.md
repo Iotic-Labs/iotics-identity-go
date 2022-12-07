d1bf35d767dcdfced4c767fba27de4f195a9b3c0
smartrics <fabrizio.cannizzo@iotic-labs.com>
2022-12-07 22:45:51 +0000

    refactored resolver client

---
+++ ffi/java-idsdk/RELEASES.md
@@ -0,0 +1,7 @@
+# 1.1
+
+- Refactored ResolverClient
+
+# 1.0
+
+- Initial release
---
+++ ffi/java-idsdk/src/main/java/com/iotics/sdk/identity/experimental/HttpResolverClient.java
@@ -0,0 +1,72 @@
+package com.iotics.sdk.identity.experimental;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Base64;
+import java.util.Objects;
+
+public final class HttpResolverClient implements ResolverClient {
+    private final URL base;
+    private final OkHttpClient client;
+
+    public HttpResolverClient(URL base) {
+        this.base = base;
+        this.client = new OkHttpClient();
+    }
+
+    public Result discover(String did) throws IOException {
+        URL url = null;
+        try {
+            url = new URL(base, "/1.0/discover/" + did);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("invalid did");
+        }
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            ResponseBody body = response.body();
+            if(response.code() > 299) {
+                if(response.code() == 404) {
+                    return new Result("DID not found", "application/text", true);
+                }
+                if(body != null) {
+                    return new Result(body.string(), "application/xml", true);
+                }
+                else {
+                    return new Result("No result found", "application/text", true);
+                }
+            }
+            if(body == null) {
+                return new Result("invalid response", "application/text", true);
+            }
+            JSONObject obj = new JSONObject(body.string());
+            String token = obj.getString("token");
+            Base64.Decoder decoder = Base64.getDecoder();
+            String payload = new String(decoder.decode(token.split("\\.")[1]));
+            obj = new JSONObject(payload);
+            return new Result(obj.toString(3), "application/json", false);
+        }
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        HttpResolverClient c = new HttpResolverClient(URI.create("https://did.stg.iotics.com").toURL());
+        Result agent = c.discover("did:iotics:iotJxn2AHBkaFXKkBymbFYcVokGhLShLtUf1");
+        Result user = c.discover("did:iotics:iotLUmwHDFtpfLEWTeGAQwyp4Y5FoSTt4jbg");
+
+        System.out.println("AGENT ------");
+        System.out.println(agent);
+        System.out.println("USER ------");
+        System.out.println(user);
+    }
+}
