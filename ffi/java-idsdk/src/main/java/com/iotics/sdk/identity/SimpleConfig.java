b06a12a528251344fdcf14787a8b5fb92956a685
smartrics <fabrizio.cannizzo@iotic-labs.com>
2022-12-11 23:41:46 +0000

    added ability to read from env

--- ffi/java-idsdk/src/main/java/com/iotics/sdk/identity/SimpleConfig.java
+++ ffi/java-idsdk/src/main/java/com/iotics/sdk/identity/SimpleConfig.java
@@ -1,5 +1,6 @@
 package com.iotics.sdk.identity;
 
+import com.google.common.base.Strings;
 import com.google.common.io.Files;
 import com.google.gson.Gson;
 
@@ -14,10 +15,30 @@ public class SimpleConfig {
     private String seed;
     private String keyName;
 
+    public static SimpleConfig fromEnv(String prefix) {
+        SimpleConfig sc = new SimpleConfig();
+        sc.seed = System.getenv(prefix + "SEED");
+        sc.keyName = System.getenv(prefix + "KEYNAME");
+        return sc;
+    }
+
     public static SimpleConfig readConf(Path p) throws FileNotFoundException {
         Gson gson = new Gson();
         Reader reader = Files.newReader(p.toFile(), Charset.forName("UTF-8"));
         return gson.fromJson(reader, SimpleConfig.class);
+    }
+
+    public static SimpleConfig readConf(Path p, SimpleConfig def) throws FileNotFoundException {
+        if(def == null) {
+            throw new IllegalArgumentException("null default");
+        }
+        Gson gson = new Gson();
+        Reader reader = Files.newReader(p.toFile(), Charset.forName("UTF-8"));
+        SimpleConfig sc = gson.fromJson(reader, SimpleConfig.class);
+        if(sc == null) {
+            return def;
+        }
+        return sc.cloneWithDefaults(def);
     }
 
     private static SimpleConfig readConfFromHome(String name) throws FileNotFoundException {
@@ -31,5 +52,22 @@ public class SimpleConfig {
 
     public String keyName() {
         return keyName;
+    }
+
+    public boolean isValid() {
+        return !Strings.isNullOrEmpty(this.seed) && !Strings.isNullOrEmpty(this.keyName) ;
+    }
+
+    private SimpleConfig cloneWithDefaults(SimpleConfig def) {
+        SimpleConfig sc = new SimpleConfig();
+        sc.seed = this.seed;
+        if(sc.seed == null) {
+            sc.seed = def.seed;
+        }
+        sc.keyName = this.keyName;
+        if(sc.keyName == null) {
+            sc.keyName = def.keyName;
+        }
+        return sc;
     }
 }
