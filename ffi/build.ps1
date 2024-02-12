9347f06eaa88bc60600d944a3e597c6271558781
Fabrizio Cannizzo <fabrizio.cannizzo@iotics.com>
2024-02-12 19:23:56 +0000

    fixed comment and env var name as per review

--- ffi/build.ps1
+++ ffi/build.ps1
@@ -1,5 +1,5 @@
-# Set the name of your Go program
-$programName = "lib-iotics-id-sdk"
+# Set the name of your Go library
+$libName = "lib-iotics-id-sdk"
 
 # Set the source file name (assuming it's in the same directory as the script)
 $sourceFile = "."
@@ -13,7 +13,7 @@ $env:CGO_ENABLED=1
 # Compile for Windows
 Write-Host "Compiling for Windows..."
 $env:GOOS="windows"
-go build -x -v -buildmode=c-shared -o "$outputDirectory\$programName.dll" $sourceFile
+go build -x -v -buildmode=c-shared -o "$outputDirectory\$libName.dll" $sourceFile
 
 if ($LASTEXITCODE -ne 0) {
     Write-Host "Error: Compilation for Windows failed with exit code $LASTEXITCODE"
